f8fc55a332bfb0c265d1d4e8252922f8f14e7d5d
Larissa Hikari Ono <lari.hk.ono@gmail.com>
2024-06-12 17:24:21 -0300

    inserir metodo render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+// Componente em classe é uma classe que herda a classe Component do React, e retorna HTML dentro do método render. 
 class App extends React.Component {
-  render();
+  render() {
+    return (
+      
+    );
+  };
 }
 
 export default App;
